e2c92102c68ffb58f77be3a7a8f2d21473799298
keoki.lee <keoki.lee@f267d017-2828-0410-b1b7-fba549515e57>
2007-02-07 07:36:34 +0000

    Checked in initial set.  Pages still do not display and I don't know why.

---
+++ trunk/src/org/hawaii/edu/photohunt/webapp/beans/PictureBean.java
@@ -0,0 +1,11 @@
+package org.hawaii.edu.photohunt.webapp.beans;
+
+/**
+ * The PictureBean class is the bean that backs the pictures in the webapp.
+ * 
+ * @author George Lee
+ *
+ */
+public class PictureBean {
+
+}
---
+++ trunk/webapp/slideshow.jsp
@@ -0,0 +1,3 @@
+<%@ include file="Header.jsp" %>
+
+This is where the slide show will be.  There will be next and previous links at the top.
