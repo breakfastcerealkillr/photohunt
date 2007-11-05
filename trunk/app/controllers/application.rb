a51666716afd2443822309a1d7ca5931fa5972a2
keoki.lee <keoki.lee@f267d017-2828-0410-b1b7-fba549515e57>
2007-11-05 20:13:42 +0000

    Initial app folder upload.  Implements the login action.

--- trunk/app/controllers/application.rb
+++ trunk/app/controllers/application.rb
@@ -4,4 +4,14 @@
 class ApplicationController < ActionController::Base
   # Pick a unique cookie name to distinguish our session data from others'
   session :session_key => '_photohunt-rails_session_id'
+  
+  private
+  
+  def authorize
+    unless User.find_by_id(:session[:user_id])
+      session[:original_uri] = request.request_uri
+      flash[:notice] = "This action requires you to log in."
+      redirect_to(:controller => "login", :action => "login")
+    end
+  end
 end
